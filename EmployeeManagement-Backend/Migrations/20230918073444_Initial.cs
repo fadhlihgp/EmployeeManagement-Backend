c9fbc66687e7d59ebb07cea1acc0a543ba4b6667
fadhlihgp <fadhlihgp@gmail.com>
2023-09-18 14:47:32 +0700

    Initial

---
+++ EmployeeManagement-Backend/Migrations/20230918073444_Initial.cs
@@ -0,0 +1,391 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EmployeeManagement_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AttendanceCode",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "Varchar(50)", nullable: false),
+                    Name = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AttendanceCode", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Company",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "Varchar(50)", nullable: false),
+                    Name = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Address = table.Column<string>(type: "Text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Company", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "projectStatus",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Name = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_projectStatus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Role",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Name = table.Column<string>(type: "Varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "Varchar(50)", nullable: false),
+                    IdentityNumber = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Name = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "varchar(50)", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ImageUrl = table.Column<string>(type: "varchar(50)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    JoinDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CompanyId = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employee_Company_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Company",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(50)", nullable: false),
+                    ProjectCode = table.Column<string>(type: "varchar(50)", nullable: false),
+                    ProjectName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    ClientName = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LongDay = table.Column<int>(type: "integer", nullable: false),
+                    CompanyId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    ProjectStatusId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Project_Company_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Company",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Project_projectStatus_ProjectStatusId",
+                        column: x => x.ProjectStatusId,
+                        principalTable: "projectStatus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "Varchar(50)", nullable: false),
+                    FullName = table.Column<string>(type: "varchar(255)", nullable: false),
+                    UserName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", maxLength: 2147483647, nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    RoleId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    CompanyId = table.Column<string>(type: "varchar(50)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Account_Company_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Company",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Account_Role_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Role",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Attendance",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "Varchar(50)", nullable: false),
+                    EmployeeId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AttendanceCodeId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    CompanyId = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attendance", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Attendance_AttendanceCode_AttendanceCodeId",
+                        column: x => x.AttendanceCodeId,
+                        principalTable: "AttendanceCode",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Attendance_Company_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Company",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Attendance_Employee_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employee",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TR_Employee_Project",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(50)", nullable: false),
+                    ProjectId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    EmployeeId = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TR_Employee_Project", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TR_Employee_Project_Employee_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employee",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TR_Employee_Project_Project_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Project",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LoginHistory",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(50)", nullable: false),
+                    AccountId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    LastLogin = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoginHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LoginHistory_Account_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Account",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AttendanceCode",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { "A1", "Sakit" },
+                    { "A2", "Izin" },
+                    { "A3", "Cuti" },
+                    { "A4", "Alpa" },
+                    { "A5", "Lainya" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { "1", "SuperAdmin" },
+                    { "2", "Owner" },
+                    { "3", "Admin" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "projectStatus",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { "P1", "Perencanaan" },
+                    { "P2", "Analisis" },
+                    { "P3", "Sedang Berlangsung" },
+                    { "P4", "Selesai" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Account",
+                columns: new[] { "Id", "Address", "CompanyId", "Email", "FullName", "Password", "RoleId", "UserName" },
+                values: new object[] { "00000000-0000-0000-0000-000000000000", "Bekasi", null, "superadmin@email.com", "Super Admin", "$2a$11$vRuGfcDB.4zEp.rpYGE3TumF3SUs3mzIXAlDRYwyEYyQ/yqzCCczm", "1", "SuperAdmin" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_CompanyId",
+                table: "Account",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_Email",
+                table: "Account",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_RoleId",
+                table: "Account",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_UserName",
+                table: "Account",
+                column: "UserName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attendance_AttendanceCodeId",
+                table: "Attendance",
+                column: "AttendanceCodeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attendance_CompanyId",
+                table: "Attendance",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attendance_EmployeeId",
+                table: "Attendance",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Company_Email",
+                table: "Company",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Company_PhoneNumber",
+                table: "Company",
+                column: "PhoneNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_CompanyId",
+                table: "Employee",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_PhoneNumber",
+                table: "Employee",
+                column: "PhoneNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LoginHistory_AccountId",
+                table: "LoginHistory",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_CompanyId",
+                table: "Project",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_ProjectStatusId",
+                table: "Project",
+                column: "ProjectStatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TR_Employee_Project_EmployeeId",
+                table: "TR_Employee_Project",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TR_Employee_Project_ProjectId",
+                table: "TR_Employee_Project",
+                column: "ProjectId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attendance");
+
+            migrationBuilder.DropTable(
+                name: "LoginHistory");
+
+            migrationBuilder.DropTable(
+                name: "TR_Employee_Project");
+
+            migrationBuilder.DropTable(
+                name: "AttendanceCode");
+
+            migrationBuilder.DropTable(
+                name: "Account");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Project");
+
+            migrationBuilder.DropTable(
+                name: "Role");
+
+            migrationBuilder.DropTable(
+                name: "Company");
+
+            migrationBuilder.DropTable(
+                name: "projectStatus");
+        }
+    }
+}
